990ddbb3ac451e4757e9e0ba99fb7518655a5c4c
ludoff <ludovicofiore25@gmail.com>
2024-09-20 12:47:22 +0200

    feat: separazione ambienti admin e guest

---
+++ resources/views/guest/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.guest')
+
+@section('content')
+    <h2>pagina del sito pubblico</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +16,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// rotte admin in dashboard controller
+Route::middleware(['auth','verified'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function(){
+        Route::get('/', [DashboardController::class, 'index'])->name('home');
+    });
+
 
 require __DIR__.'/auth.php';
